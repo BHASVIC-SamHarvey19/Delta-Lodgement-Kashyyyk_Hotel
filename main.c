0ebacf6356fff1ebff89eccd4cabeec4676a4711
BHASVIC-SamHarvey19 <sam.harvey24@students.bhasvic.ac.uk>
2024-11-11 13:42:43 +0000

    Changes

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int Welcoming(void);
+
+int Welcoming(void){
+    printf("~~~WELCOME TO KASHYYYK HOTEL~~~ \n");
+    printf("Would you like to ");
+}
+
+
 
 int main(void) {
-    printf("uysdfgfuyhsd\n");
+
     return 0;
 }
